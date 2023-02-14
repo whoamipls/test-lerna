e287540031eb83f8760f46f2c230b36d15807c77
dongrui.wang <dongrui.wang@mapabc.com>
2023-02-14 17:11:45 +0800

    1

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -2,7 +2,7 @@
  * @Author: wangdongrui
  * @Date: 2023-02-14 10:51:49
  * @LastEditors: wangdongrui
- * @LastEditTime: 2023-02-14 17:07:14
+ * @LastEditTime: 2023-02-14 17:11:42
  * @FilePath: \lerna\packages\core\lib\core.js
  * @Description: 
  */
@@ -11,5 +11,5 @@
 module.exports = core;
 
 function core() {
-    return "Hello from core 1";
+    return "Hello from core 2";
 }
